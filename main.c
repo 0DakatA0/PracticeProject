3897ab9c25b54d31ac48a4690c981b4e679b1290
Atanas Atanasov <nasko200406@gmail.com>
2021-06-08 18:34:56 +0300

    UPDATE menu

--- main.c
+++ main.c
@@ -14,38 +14,41 @@ int dayTime() {
 }
 
 void menu() {
-    unsigned int user_choice = 0;
 
-    printf("Choose one of the following options:\n");
-    printf("\n1.Login (Enter 1)\n");
-    printf("2.Registration (Enter 2)\n");
-    printf("3.Exit (Enter 0)\n");
+    while (1) {
+        unsigned int user_choice = 0;
 
-    do {
-        printf("\nEnter here: ");
-        scanf("%d", &user_choice);
+        printf("\nChoose one of the following options:\n");
+        printf("\n1.Login (Enter 1)\n");
+        printf("2.Registration (Enter 2)\n");
+        printf("3.Exit (Enter 0)\n");
 
-        if (user_choice > 2) {
-            printf("\nInvalid choice!\n");
-        }
+        do {
+            printf("\nEnter here: ");
+            scanf("%d", &user_choice);
 
-    } while (user_choice > 2);
-
-    switch (user_choice) {
-        case 1:
-            printf("Login\n"); //Here will be the login func
-            break; 
-        case 2:
-            printf("Registration\n"); // Here will be the registration func
-            break;
-        case 0:
-            if (dayTime() == 1) {
-                printf("Have a nice evening.");
-            } else {
-                printf("Have a nice day.");
+            if (user_choice > 2) {
+                printf("\nInvalid choice!\n");
             }
 
-            return;
+        } while (user_choice > 2);
+
+        switch (user_choice) {
+            case 1:
+                printf("Login\n"); //Here will be the login func
+                break; 
+            case 2:
+                printf("Registration\n"); // Here will be the registration func
+                break;
+            case 0:
+                if (dayTime() == 1) {
+                    printf("Have a nice evening.");
+                } else {
+                    printf("Have a nice day.");
+                }
+
+                return;
+        }
     }
 }
 
