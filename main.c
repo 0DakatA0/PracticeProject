64bdf25583894a7a2d50e7f2e2105de6c8bfabf2
Lacho <61554888+ZariProjects@users.noreply.github.com>
2021-06-08 19:15:07 +0300

    Add String Based Case Insensitive Commands to Menu

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 int dayTime() {
     time_t t = time(NULL);
@@ -13,40 +15,53 @@ int dayTime() {
     return 0;
 }
 
+char* stabilize_letter_case(char* menu_option_string) {
+    unsigned char *menu_option_string_pointer = (unsigned char *)menu_option_string;
+
+    while (*menu_option_string_pointer) {
+        *menu_option_string_pointer = tolower((unsigned char)*menu_option_string_pointer);
+        menu_option_string_pointer++;
+    }
+
+    return menu_option_string;
+}
+ 
+int validate(char* menu_option_string) {
+    menu_option_string = stabilize_letter_case(menu_option_string);
+    if(
+        strcmp(menu_option_string, "sign in") == 0 ||
+        strcmp(menu_option_string, "sign up") == 0 ||
+        strcmp(menu_option_string, "exit") == 0 ||
+        strcmp(menu_option_string, "help") == 0
+    ) return 1;
+    return 0;
+}
+
 void menu() {
-    unsigned int user_choice = 0;
+    char menu_option_string[50] = {0};
+    int is_menu_option_valid = 0;
 
     printf("Choose one of the following options:\n");
-    printf("\n1.Login (Enter 1)\n");
-    printf("2.Registration (Enter 2)\n");
-    printf("3.Exit (Enter 0)\n");
+    printf("\n1.Sign In [sign in]\n2.Sign Up [sign up] \n3.Exit [exit] \n4.Help [help]\n");
 
     do {
-        printf("\nEnter here: ");
-        scanf("%d", &user_choice);
+        printf("\n: ");
+        scanf("%s", menu_option_string);
 
-        if (user_choice > 2) {
-            printf("\nInvalid choice!\n");
+        is_menu_option_valid = validate(menu_option_string);
+        if(!is_menu_option_valid) {
+            printf("Option not found. Type help for info.\n");
         }
+        printf("%s", menu_option_string);
 
-    } while (user_choice > 2);
+    } while (!is_menu_option_valid);
 
-    switch (user_choice) {
-        case 1:
-            printf("Login\n"); //Here will be the login func
-            break; 
-        case 2:
-            printf("Registration\n"); // Here will be the registration func
-            break;
-        case 0:
-            if (dayTime() == 1) {
-                printf("Have a nice evening.");
-            } else {
-                printf("Have a nice day.");
-            }
-
-            return;
+    if(strcmp(menu_option_string, "help") == 0) {
+        printf("\nUsage [Commands]: \n   sign in (Case Insensitive) \n   sign up (Case Insensitive) \n   exit (Case Insensitive) \n   help (Case Insensitive)");
     }
+    
+
+    printf("YO");
 }
 
 int main() {
